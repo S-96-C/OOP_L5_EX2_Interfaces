9877049f2a91b77664e2c564de73880bdf041950
Shehan Cooray <109017782+S-96-C@users.noreply.github.com>
2022-08-13 22:12:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,65 @@
+//Interface
+
+interface ICompute { 
+	
+	void calculate();
+	
+	void display(); 
+} 
+
+class Person implements ICompute{ 
+	
+	private String name;
+	private double basicSal; 
+	private double otRate;
+	private double otHrs; 
+	private double netSal; 
+	
+	public Person(String name, double basicSal, double otRate, double otHrs) {
+		this.name = name;
+		this.basicSal = basicSal;
+		this.otRate = otRate;
+		this.otHrs = otHrs;
+	}
+	
+	public void calculate() {
+		netSal = basicSal + (otHrs * otRate);
+	}
+	
+	public void display() {
+		System.out.println(name);
+		System.out.println("net salary is : " +netSal);
+	}
+} 
+
+class Box implements ICompute  { 
+	private int length, width, height; 
+	private int volume; 
+	
+	public Box(int length, int width, int height) {
+		this.length = length;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public void calculate() {
+		volume = length * width * height;
+	}
+	
+	public void display() {
+		System.out.println("Volume is :" +volume);
+	}
+}
+
+class Main { 
+	public static void main(String[] args) { 
+		
+		ICompute ob1 = new Person("Danushka",40000, 1000, 5); 
+		ob1.calculate(); ob1.display();
+		ob1.display();
+		
+		ICompute ob2 = new Box(10, 20, 30); 
+		ob2.calculate(); 
+		ob2.display(); 
+	} 
+}
